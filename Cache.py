9d952a56f24f77a395a31a94f755e99a791000b9
雨落倾城 <2199974716@qq.com>
2025-04-21 23:08:28 +0800

    添加语义缓存

--- Cache.py
+++ Cache.py
@@ -6,11 +6,29 @@ os.environ["SERPAPI_API_KEY"] = os.getenv("SERPAPI_KEY")
 
 import langchain
 from langchain.cache import InMemoryCache
-from langchain.chat_models import ChatOpenAI
+from langchain.cache import GPTCache
+
+from gptcache import Cache
+from gptcache.adapter.api import init_similar_cache
+from gptcache.embedding import OpenAI as OpenAIEmbedding
+from langchain_openai import ChatOpenAI
+from langchain.callbacks import StreamingStdOutCallbackHandler
 import time
 
+def init_gptcache(cache_obj: Cache, llm_string: str):
+    init_similar_cache(
+        cache_obj=cache_obj,
+        embedding_func=OpenAIEmbedding(),
+    )
+
 langchain.llm_cache = InMemoryCache()
-llm = ChatOpenAI(base_url="https://api.chatanywhere.tech/v1")
+# langchain.llm_cache = GPTCache(init_gptcache)
+llm = ChatOpenAI(
+    base_url="https://api.chatanywhere.tech/v1", 
+    streaming=True, 
+    callbacks=[StreamingStdOutCallbackHandler()],
+)
+
 start_time = time.time()
 print(llm.invoke("你是谁？").content)
 print("第一次调用耗时：", time.time() - start_time)
