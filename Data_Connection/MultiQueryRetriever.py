9cf883da84c28171b3014882458ff143bba46798
雨落倾城 <2199974716@qq.com>
2025-04-23 21:24:16 +0800

    上下文压缩检索器

--- Data_Connection/MultiQueryRetriever.py
+++ Data_Connection/MultiQueryRetriever.py
@@ -11,6 +11,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.retrievers import MultiQueryRetriever
 import shutil
 from langchain.chat_models import ChatOpenAI
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
 
 loader = WebBaseLoader(
     'https://www.yixue.com/%E7%96%BE%E7%97%85%E7%9A%84%E5%88%86%E7%B1%BB'
@@ -35,23 +37,51 @@ vectordb = Chroma.from_documents(
     persist_directory="./chroma_test/",
 )
 
-# 初始化检索器
+# # 初始化检索器
+# llm = ChatOpenAI(base_url="https://api.chatanywhere.tech/v1")
+# retriever_from_llm = MultiQueryRetriever.from_llm(
+#     retriever=vectordb.as_retriever(),
+#     llm=llm,
+# )
+
+# # 设置日志级别
+# logging.basicConfig()
+# logging.getLogger(
+#     'langchain.retrievers.multi_query'
+# ).setLevel(logging.INFO)
+
+# # 进行检索
+# docs = retriever_from_llm.get_relevant_documents(
+#     query="什么是遗传病？"
+# )
+# for doc in docs:
+#     print(doc.page_content)
+#     print("===" * 10)
+
+retriever = vectordb.as_retriever()
+docs = retriever.get_relevant_documents(
+    query="疾病有哪些类型？"
+)
+
+def print_docs(docs):
+    doc_str_list = [
+        f'Document {i+1}:\n\n{d.page_content}\n\n' for i, d in enumerate(docs)
+    ]
+    print(f"\n{'-'*99}\n".join(doc_str_list))
+
+print('原始文档：')
+print_docs(docs)
+
 llm = ChatOpenAI(base_url="https://api.chatanywhere.tech/v1")
-retriever_from_llm = MultiQueryRetriever.from_llm(
-    retriever=vectordb.as_retriever(),
-    llm=llm,
+compressor = LLMChainExtractor.from_llm(llm)
+compressor_retriever = ContextualCompressionRetriever(
+    base_compressor=compressor,
+    base_retriever=retriever
 )
 
-# 设置日志级别
-logging.basicConfig()
-logging.getLogger(
-    'langchain.retrievers.multi_query'
-).setLevel(logging.INFO)
-
-# 进行检索
-docs = retriever_from_llm.get_relevant_documents(
-    query="什么是遗传病？"
+docs = compressor_retriever.get_relevant_documents(
+    query="疾病有哪些类型？"
 )
-for doc in docs:
-    print(doc.page_content)
-    print("===" * 10)
+
+print('总结后的文档：')
+print_docs(docs)
